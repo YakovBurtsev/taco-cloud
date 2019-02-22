3209b8329a81b698452a95a0dfe7923e2cab7b56
yburtsev <yakovburtsev@gmail.com>
2019-02-22 20:13:58 +0300

    fix keyHolder.getKey().longValue() NPE by setReturnGeneratedKeys(true) for PreparedStatementCreatorFactory

--- src/main/java/tacos/data/JdbcTacoRepository.java
+++ src/main/java/tacos/data/JdbcTacoRepository.java
@@ -36,11 +36,15 @@ public class JdbcTacoRepository implements TacoRepository {
 
     private long saveTacoInfo(Taco taco) {
         taco.setCreatedAt(new Date());
-        PreparedStatementCreator psc =
-                new PreparedStatementCreatorFactory(
-                        "insert into Taco (name, createdAt) values (?, ?)",
-                        Types.VARCHAR, Types.TIMESTAMP
-                ).newPreparedStatementCreator(
+        PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+                "insert into Taco (name, createdAt) values (?, ?)",
+                Types.VARCHAR, Types.TIMESTAMP
+        );
+
+        pscf.setReturnGeneratedKeys(true);
+
+        PreparedStatementCreator psc = pscf
+                .newPreparedStatementCreator(
                         Arrays.asList(
                                 taco.getName(),
                                 new Timestamp(taco.getCreatedAt().getTime())));
