9b3a8fceaf57192d33dc3f136e12bcdc8b23b8ae
yakov.burtsev <yakovburtsev@gmail.com>
2019-03-11 00:02:05 +0300

    registration form validation

--- src/main/java/tacos/security/RegistrationController.java
+++ src/main/java/tacos/security/RegistrationController.java
@@ -3,11 +3,15 @@ package tacos.security;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.User;
 import tacos.data.UserRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/register")
@@ -26,8 +30,19 @@ public class RegistrationController {
         return "registration";
     }
 
+    @ModelAttribute(name = "registrationForm")
+    public RegistrationForm registrationForm() {
+        return new RegistrationForm();
+    }
+
     @PostMapping
-    public String processRegistration(RegistrationForm form, Model model) {
+    public String processRegistration(@Valid RegistrationForm form, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "registration";
+        }
+
+        //todo: check password == confirm
         User user = form.toUser(passwordEncoder);
 
         if (userRepo.findByUsername(user.getUsername()) != null) {
--- src/main/java/tacos/security/RegistrationForm.java
+++ src/main/java/tacos/security/RegistrationForm.java
@@ -4,16 +4,38 @@ import lombok.Data;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.User;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 class RegistrationForm {
 
+    @NotNull
+    @Size(min = 5, message = "Username must be at least 5 characters long")
     private String username;
+
+    @NotNull
+    @Size(min = 5, message = "Password must be at least 5 characters long")
     private String password;
+
+    private String confirm;
+
     private String fullname;
+
+    @NotBlank(message="Street is required")
     private String street;
+
+    @NotBlank(message="City is required")
     private String city;
+
+    @NotBlank(message="State is required")
     private String state;
+
+    @NotBlank(message="Zip code is required")
     private String zip;
+
+    @NotBlank(message="Phone is required")
     private String phone;
 
     User toUser(PasswordEncoder passwordEncoder) {
