afa2301780202f4511178cf738bf788658d92308
yakov.burtsev <yakovburtsev@gmail.com>
2019-03-10 16:37:48 +0300

    fix tests

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -26,9 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/design", "/orders", "/")
+                .antMatchers("/design", "/orders")
                 .access("hasRole('ROLE_USER')")
-                .antMatchers("/**").access("permitAll")
+                .antMatchers("/", "/**").access("permitAll")
 
                 .and()
                 .formLogin()
--- src/test/java/tacos/web/HomeControllerTest.java
+++ src/test/java/tacos/web/HomeControllerTest.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.data.IngredientRepository;
+import tacos.data.OrderRepository;
+import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +25,27 @@ public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+
+    // Note: Most of these mocks are here to avoid autowiring issues. They aren't
+    //       actually used in the course of the home page test, so their behavior
+    //       isn't important. They just need to exist so autowiring can take place.
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private TacoRepository designRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
 
     @Test
     public void testHomePage() throws Exception {
