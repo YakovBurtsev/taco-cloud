66e5d4bce8ae9e0afb22965296b03dc481eb191d
yakov.burtsev <yakovburtsev@gmail.com>
2019-03-14 00:18:53 +0300

    add default user in case !prod profile active

--- src/main/java/tacos/DevConfig.java
+++ src/main/java/tacos/DevConfig.java
@@ -4,25 +4,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.data.IngredientRepository;
+import tacos.data.UserRepository;
 
 @Profile("!prod")
 @Configuration
 public class DevConfig {
 
     @Bean
-    public CommandLineRunner dataLoader(IngredientRepository repo) {
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository, UserRepository userRepository, PasswordEncoder encoder) {
         return args -> {
-            repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-            repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+            ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+            ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+            ingredientRepository.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+            ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+            ingredientRepository.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+            ingredientRepository.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+            ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+            ingredientRepository.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+            ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+
+            userRepository.save(new User("yakov", encoder.encode("yakov"), "Yakov Burtsev",
+                    "Some street", "Some city", "Some state", "123456", "123-123-1234"));
         };
     }
 }
