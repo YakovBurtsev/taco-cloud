4ddeaf79d519b75d8a5ced41164770fd124c4b50
yburtsev <yakovburtsev@gmail.com>
2019-03-26 11:29:35 +0300

    Add ErrorViewResolver bean to avoid 404s when using Angular HTML 5 routing

--- taco-cloud-main/src/main/java/tacos/DevConfig.java
+++ taco-cloud-main/src/main/java/tacos/DevConfig.java
@@ -6,30 +6,64 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 import tacos.data.UserRepository;
 import tacos.domain.Ingredient;
+import tacos.domain.Ingredient.Type;
+import tacos.domain.Taco;
 import tacos.domain.User;
+
+import static java.util.Arrays.asList;
 
 @Profile("!prod")
 @Configuration
 public class DevConfig {
 
     @Bean
-    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository, UserRepository userRepository, PasswordEncoder encoder) {
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository,
+                                        UserRepository userRepository,
+                                        PasswordEncoder encoder,
+                                        TacoRepository tacoRepository) {
         return args -> {
-            ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-            ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-            ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-            ingredientRepository.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-            ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-            ingredientRepository.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-            ingredientRepository.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-            ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-            ingredientRepository.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-            ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            Ingredient flourTortilla = new Ingredient("FLTO", "Flour Tortilla", Type.WRAP);
+            Ingredient cornTortilla = new Ingredient("COTO", "Corn Tortilla", Type.WRAP);
+            Ingredient groundBeef = new Ingredient("GRBF", "Ground Beef", Type.PROTEIN);
+            Ingredient carnitas = new Ingredient("CARN", "Carnitas", Type.PROTEIN);
+            Ingredient tomatoes = new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES);
+            Ingredient lettuce = new Ingredient("LETC", "Lettuce", Type.VEGGIES);
+            Ingredient cheddar = new Ingredient("CHED", "Cheddar", Type.CHEESE);
+            Ingredient jack = new Ingredient("JACK", "Monterrey Jack", Type.CHEESE);
+            Ingredient salsa = new Ingredient("SLSA", "Salsa", Type.SAUCE);
+            Ingredient sourCream = new Ingredient("SRCR", "Sour Cream", Type.SAUCE);
+
+            ingredientRepository.save(flourTortilla);
+            ingredientRepository.save(cornTortilla);
+            ingredientRepository.save(groundBeef);
+            ingredientRepository.save(carnitas);
+            ingredientRepository.save(tomatoes);
+            ingredientRepository.save(lettuce);
+            ingredientRepository.save(cheddar);
+            ingredientRepository.save(jack);
+            ingredientRepository.save(salsa);
+            ingredientRepository.save(sourCream);
 
             userRepository.save(new User("yakov", encoder.encode("yakov"), "Yakov Burtsev",
                     "Some street", "Some city", "Some state", "123456", "123-123-1234"));
+
+            Taco taco1 = new Taco();
+            taco1.setName("Carnivore");
+            taco1.setIngredients(asList(flourTortilla, groundBeef, carnitas, sourCream, salsa, cheddar));
+            tacoRepository.save(taco1);
+
+            Taco taco2 = new Taco();
+            taco2.setName("Bovine Bounty");
+            taco2.setIngredients(asList(cornTortilla, groundBeef, cheddar, jack, sourCream));
+            tacoRepository.save(taco2);
+
+            Taco taco3 = new Taco();
+            taco3.setName("Veg-Out");
+            taco3.setIngredients(asList(flourTortilla, cornTortilla, tomatoes, lettuce, salsa));
+            tacoRepository.save(taco3);
         };
     }
 }
